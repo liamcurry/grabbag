89c5d72f3443a810716ebed1245d22c27ad33706
Bill Curry <bill@curry.name>
2011-09-29 12:24:38 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='grabbag',
+      version='0.1',
+      install_requires=[
+        'jinga2',
+        'jingo',
+        'django-session-csrf'
+      ],
+)
